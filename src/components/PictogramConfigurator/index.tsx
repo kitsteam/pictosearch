64e6886a0672eb42d02313297a075aa7f61219b7
Tim Krieger <krieger@nlq.nibis.de>
2021-09-02 14:57:25 +0200

    Update licence

--- src/components/PictogramConfigurator/index.tsx
+++ src/components/PictogramConfigurator/index.tsx
@@ -206,7 +206,7 @@ const PictogramConfigurator: React.FC<Props> = (props) => {
           <Typography m={2} variant="body2" sx={{ opacity: 0.6 }}>&copy;{' '}
             <Trans i18nKey="config.license">Das abgebildete piktographische Symbol ist Eigentum der
               Regierung von Aragón und wurden von Sergio Palao für <Link href="http://www.arasaac.org">ARASAAC</Link> erstellt,
-              das sie unter der <Link href="https://creativecommons.org/licenses/by-nc-sa/3.0/de/">Creative-Commons-Lizenz BY-NC-SA</Link> weitergibt.
+              das sie unter der <Link href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en">Creative-Commons-Lizenz BY-NC-SA 4.0</Link> weitergibt.
             </Trans>
           </Typography>
 
