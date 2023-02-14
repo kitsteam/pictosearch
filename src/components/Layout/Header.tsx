7ed870c23e84df11a85eaa521b3e63976a805154
Jannik Streek <jannik.streek@gmail.com>
2023-02-14 12:34:15 +0100

    add disclaimer

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -8,21 +8,13 @@ const Header: React.FC = () => {
         <>
           <Box textAlign='left' className="app-header-left">
             <Link to="/" style={{ display: 'inline-block' }}>
-                <h1 className="header-kits-h">PictoSearch</h1>
+            <h1 className="header-kits-h"><img src="/assets/kits-logo.svg" alt="Logo Kits"/> PictoSearch</h1>
             </Link>
           </Box>
           <Box textAlign='right' className="app-header-right">
-          <Box className="app-footer">
-            <Link to="http://www.arasaac.org">
-              <img src="/assets/logo_arasaac.svg" alt="Logo ARASSAC"/>
+          <Link to="http://www.arasaac.org">
+              <img src="/assets/logo_arasaac.svg" alt="Logo ARASSAC" />
             </Link>
-            <Typography variant="body2" sx={{ opacity: 0.6, paddingLeft: 2 }}>
-              <Trans i18nKey="search.license">
-                Sergio Palao (Urheber), ARASAAC (<Link to="http://www.arasaac.org">arasaac.org</Link>),
-                Regierung von Aragón in Spanien (Eigentümer), <Link to="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en">CC BY-SA-NC 4.0</Link>
-              </Trans>
-            </Typography>
-        </Box>
           </Box>
         </>
     )
--- src/components/PictogramCollection/index.tsx
+++ src/components/PictogramCollection/index.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Stack } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link, Link as RouterLink } from 'react-router-dom';
 import BackIcon from '@mui/icons-material/ArrowLeft';
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { useCollection } from "../../hooks/collection";
 import List from "./List";
 
@@ -20,7 +20,13 @@ const PictogramCollection: React.FC<Props> = () => {
                 <Button component={RouterLink} to="/search" variant="outlined" size="small" startIcon={<BackIcon />}>{t('browse')}</Button>
             </Stack>
             <List collection={collection} />
-        </Box>
+            <Typography variant="body2" sx={{ opacity: 0.6 }}>
+            <Trans i18nKey="search.license">
+                Sergio Palao (Urheber), ARASAAC (<Link to="http://www.arasaac.org">arasaac.org</Link>),
+                Regierung von Aragón in Spanien (Eigentümer), <Link to="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en">CC BY-SA-NC 4.0</Link>
+            </Trans>
+            </Typography>
+            </Box>
     )
 }
 
