219086964a5c1c6f7a4554d8b5515d2a212293ff
Jannik Streek <jannik.streek@gmail.com>
2025-04-07 18:25:21 +0200

    tsc now works

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,8 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_ELEMENT: string
-  readonly REACT_APP_API: string
-  readonly REACT_APP_API_IMAGES: string
+  readonly VITE_REACT_APP_API: string
+  readonly VITE_REACT_APP_API_IMAGES: string
 }
 
 interface ImportMeta {
